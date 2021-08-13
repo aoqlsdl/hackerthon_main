982e7e0f83d47fd2aa9e96ad09bbf1aa2d65e191
[aoqlsdl] <[1986004@ewhain.net]>
2021-08-13 22:33:53 +0900

    ha

--- Example/settings.py
+++ Example/settings.py
@@ -139,6 +139,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog', 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'HACKERTHON-FINAL-MAIN/blog/static')
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
